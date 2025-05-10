08a771f8b8cafe101b574abeeffe8fa6bf8da96f
NeutronC <christian.brumm@t-online.de>
2025-05-10 22:37:08 +0200

    Revert "AIR"
    This reverts commit f4a741a5044118545080a3e3754be2d3f879281e.

--- common/defines/02_spt_defines.lua
+++ common/defines/02_spt_defines.lua
@@ -30,15 +30,10 @@ NDefines.NBuildings.ANTI_AIR_SUPERIORITY_MULT = 25
 
 NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.25
 NDefines.NMilitary.PEN_VS_AVERAGE = 0.3
-
-
 NDefines.NMilitary.ENEMY_AIR_SUPERIORITY_IMPACT = 0.25
 NDefines.NMilitary.ENEMY_AIR_SUPERIORITY_DEFENSE = 0.5
 NDefines.NMilitary.ENEMY_AIR_SUPERIORITY_DEFENSE_STEEPNESS = 80
 NDefines.NMilitary.AIR_SUPPORT_BASE = 0.2
-NDefines.NAir.ANTI_AIR_ATTACK_TO_DAMAGE_REDUCTION_FACTOR = 0.4  --aa counts for 40% the DR vs CAS
-NDefines.NAir.ANTI_AIR_MAXIMUM_DAMAGE_REDUCTION_FACTOR = 0.9 --but now reduces cas DMG up to 90% (53% aa1, 70% aa2, 89% aa3 at base stats from 75% with aa1,2,3 (10.8 aa averaged over all divs in battle))
-
 
 NDefines.NNavy.CARRIER_STACK_PENALTY 									        = 4         --The most efficient is 4 carriers in combat. 5+ brings the penalty to the amount of wings in battle.
 NDefines.NNavy.CARRIER_STACK_PENALTY_EFFECT 									= 0.1      --Each carrier above the optimal amount decreases the amount of airplanes being able to takeoff by such %.
